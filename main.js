834bf7b15dc116cc6226a542177bcae56f38e552
dicachalo <dchacon871@unab.edu.co>
2022-05-23 20:50:02 -0500

    Refactor code

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const URL = 'https://api.thecatapi.com/v1/images/search';
 
-async function getCat () {
-    const response = await fetch(URL);
-    const data = await response.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url; 
+//Para que al cargar el DOM se muestre una primera imagen
+document.addEventListener('DOMContentLoaded', getCat); 
+
+
+async function getCat() {
+    try {
+        const response = await fetch(URL);
+        const data = await response.json(); //Conviertiendo la respuesta a un objeto JSON
+        const image = document.querySelector('img');
+        image.src = data[0].url; 
+    } catch (error) {
+        console.log("Error", error);
+    }
 }
 
-const input = document.querySelector('input');
-input.onclick = getCat;
+const reload = document.querySelector('#reload');
+reload.onclick = getCat;
